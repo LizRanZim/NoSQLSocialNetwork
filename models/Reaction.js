6c4d1a4483d1e4875cdbe079ef5a5341fed63cb8
LizRanZim <lrzclass22@gmail.com>
2022-11-05 15:01:46 -0400

    Add Reaction.js update Thought.js

---
+++ models/Reaction.js
@@ -0,0 +1,34 @@
+// Source of this code is 18-25 Response.js with modifications
+
+const { Schema, Types } = require('mongoose');
+
+const responseSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // use a getter method to format the timestamp on query?? See format_date function on homework 14 for ideas here?
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = responseSchema;
